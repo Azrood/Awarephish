1c2661ed8a45aa4b5af629e246479ede5eb5b9c4
Azrood <53349565+Azrood@users.noreply.github.com>
2020-04-27 18:53:22 +0000

    created models

--- awarephish/quiz/models.py
+++ awarephish/quiz/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Quiztest(models.Model): # test in mcd
+    difficulty_test = models.CharField("difficulté du test", max_length=50)
+    questions = models.ManyToManyField(Question)
+    users = models.ManyToManyField(Utilisateur)
+
+
 class Question(models.Model):
     type_question = models.CharField('Type de question', max_length=100)
     question_text = models.CharField(max_length=500)
@@ -10,26 +16,27 @@ class Question(models.Model):
     note = models.SmallIntegerField()
 
 class Reponses(models.Model):
-    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answers = models.CharField('Réponses', max_length=100)
     correct_answer = models.CharField('Réponse correct', max_length=100)
+
 
 class Utilisateur(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #usernam, mdp, email
     score_actuel = models.IntegerField()
     niveau_actuel = models.CharField('Niveau', max_length=50)
-    
 
 
 class Devoirs(models.Model):
+    users = models.ManyToManyField(Utilisateur)
     type_devoir = models.CharField("Type de devoir", max_length=100)
-    duree = models.IntegerField()
-    difficulty_devoir = models.CharField
+    duree = models.IntegerField("durée estimée")
+    difficulty_devoir = models.CharField("niveau du devoir",max_length=50)
 
 
 class Progres(models.Model):
     user = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
-    date_test = models.DateField()
+    date_test = models.DateTimeField("Date de test")
     score_test = models.IntegerField()
 
 
