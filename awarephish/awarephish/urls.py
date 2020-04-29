ac474b7b9ff112bb8fe861620e57aabc91d98efb
Azrood <53349565+Azrood@users.noreply.github.com>
2020-04-29 18:26:24 +0000

    fixed root url

--- awarephish/awarephish/urls.py
+++ awarephish/awarephish/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('quiz/',include('quiz.urls')),
+    path('',include('quiz.urls')),
     path('admin/', admin.site.urls),
     
 ]
