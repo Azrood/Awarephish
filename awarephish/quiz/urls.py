639c7efca72f3656fcb39f70e8d0e8ae57febba0
Azrood <53349565+Azrood@users.noreply.github.com>
2020-04-30 02:19:26 +0000

    added views and urls for sign up, login and logout

--- awarephish/quiz/urls.py
+++ awarephish/quiz/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.redir, name='redir'),
     path('index/', views.index,name='index'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('conseils/', views.conseils, name='conseils'),
 ]
--- awarephish/quiz/views.py
+++ awarephish/quiz/views.py
@@ -1,7 +1,54 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+
+from .forms import SignupForm, SigninForm
 
 # Create your views here.
 
+def redir(request):
+    return redirect('/index/')
+
 def index(request):
-    return HttpResponse("viewer")
+    return render(request,"quiz/index.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/index/')
+    else:
+        form = SignupForm()
+    return render(request,'quiz/signup.html/',{'form':form})
+
+def signin(request):
+    if request.method == 'POST':
+        form = SigninForm(data=request.POST)
+        if form.is_valid():
+            print(dir(form))
+            user = authenticate(
+                    username= form.cleaned_data['username'],
+                    password=form.cleaned_data['password'])
+            login(request, user)
+            return redirect('/index/')
+    else:
+        form = SigninForm()
+    return render(request, 'quiz/signin.html',{'form':form})
+
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('/index/')
+
+def conseils(request):
+    return render(request,'quiz/conseils.html')
+
+@login_required
