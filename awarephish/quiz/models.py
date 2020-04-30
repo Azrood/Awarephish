e8acaa87990c1c14146520d3b1d4317465951571
Azrood <53349565+Azrood@users.noreply.github.com>
2020-04-30 16:59:08 +0000

    model update

--- awarephish/quiz/models.py
+++ awarephish/quiz/models.py
@@ -8,7 +8,10 @@ class Question(models.Model):
     question_text = models.TextField(max_length=500)
     difficulty = models.CharField('Difficulté', max_length=10)
     note = models.SmallIntegerField()
-    image = models.ImageField(upload_to='questions', null=True)
+    image = models.ImageField(upload_to='questions', null=True, blank=True)
+
+    def __str__(self):
+        return self.question_text
 
 class Reponses(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -18,15 +21,23 @@ class Reponses(models.Model):
 class Devoir(models.Model):
     type_devoir = models.CharField("Type de devoir", max_length=100)
     difficulty_devoir = models.CharField("niveau du devoir",max_length=50)
-    duree = models.IntegerField("durée estimée")
-    contenu = models.FileField(upload_to='devoirs/%Y/%m/%d/',null=True)
-    texte = models.TextField("Texte", max_length=500, null=True)
+    duree = models.IntegerField("durée estimée (min)")
+    contenu = models.FileField(upload_to='devoirs/%Y/%m/%d/',null=True,blank=True)
+    texte = models.TextField("Texte", max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return f"Devoir {self.difficulty_devoir} : type {self.type_devoir}"
+
 
 class Utilisateur(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) #usernam, mdp, email
-    score_actuel = models.IntegerField()
-    niveau_actuel = models.CharField('Niveau', max_length=50)
-    homework = models.ManyToManyField(Devoir)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    score_actuel = models.IntegerField(default=0)
+    niveau_actuel = models.CharField('Niveau', max_length=50, default='Débutant')
+    homework = models.ManyToManyField(Devoir,blank=True)
+    total_question = models.IntegerField("nombre total de réponse", default=0)
+    total_questions_correctes = models.IntegerField("nombre de total de réponses correcte", default=0)
+    def __str__(self):
+        return str(self.user)
 
 class Quiztest(models.Model): # test in mcd
     difficulty_test = models.CharField("difficulté du test", max_length=50)
