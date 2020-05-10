52ba7f03eb7f71059b24eb063bbe88063fffc06b
Azrood <53349565+Azrood@users.noreply.github.com>
2020-05-10 22:35:40 +0000

    update model

--- awarephish/quiz/models.py
+++ awarephish/quiz/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
     type_question = models.CharField('Type de question', max_length=100)
-    question_text = models.TextField(max_length=500)
+    question_text = models.TextField(max_length=700)
     difficulty = models.CharField('Difficulté', max_length=10)
     note = models.SmallIntegerField()
     image = models.ImageField(upload_to='questions', null=True, blank=True)
@@ -15,15 +15,15 @@ class Question(models.Model):
 
 class Reponses(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    wrong_answers = models.CharField('Réponses Incorrectes', max_length=100, null=True)
-    correct_answer = models.CharField('Réponses correctes', max_length=100, null=True)
+    wrong_answers = models.CharField('Réponses Incorrectes', max_length=400, null=True)
+    correct_answer = models.CharField('Réponses correctes', max_length=400, null=True)
 
 class Devoir(models.Model):
     type_devoir = models.CharField("Type de devoir", max_length=100)
     difficulty_devoir = models.CharField("niveau du devoir",max_length=50)
     duree = models.IntegerField("durée estimée (min)")
     contenu = models.FileField(upload_to='devoirs/%Y/%m/%d/',null=True,blank=True)
-    texte = models.TextField("Texte", max_length=500, null=True, blank=True)
+    texte = models.TextField("Texte", max_length=600, null=True, blank=True)
 
     def __str__(self):
         return f"Devoir {self.difficulty_devoir} : type {self.type_devoir}"
@@ -32,7 +32,7 @@ class Devoir(models.Model):
 class Utilisateur(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     score_actuel = models.IntegerField(default=0)
-    niveau_actuel = models.CharField('Niveau', max_length=50, default='Débutant')
+    niveau_actuel = models.CharField('Niveau', max_length=50, default='Niveau 1')
     homework = models.ManyToManyField(Devoir,blank=True)
     total_reponse = models.IntegerField("nombre total de reponse", default=0)
     total_reponse_correctes = models.IntegerField("nombre de total de reponse correcte", default=0)
@@ -40,7 +40,7 @@ class Utilisateur(models.Model):
         return str(self.user)
 
 class Quiztest(models.Model): # test in mcd
-    difficulty_test = models.CharField("difficulté du test", max_length=50)
+    difficulty_test = models.CharField("Niveau du test", max_length=50)
     questions = models.ManyToManyField(Question)
     users = models.ManyToManyField(Utilisateur)
 
