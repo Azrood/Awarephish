438dddecc72b4a547458f996e9b40c2f1142388e
Azrood <53349565+Azrood@users.noreply.github.com>
2020-06-03 14:20:54 +0100

    changed level for level evaluation test

--- awarephish/quiz/utils.py
+++ awarephish/quiz/utils.py
@@ -1,3 +1,7 @@
+BEGINNER = "Débutant"
+AVERAGE = "Moyen"
+ADVANCED = "Avancé"
+
 def evaluate_level(score):
     """Evaluate level base on score"""
     assert 0<= score <=300
@@ -23,18 +27,18 @@ def get_user_answers(request):
     return [ (k if v=='on' else v) for k,v in request.POST.items() if 'csrfmiddle' not in k][:-1]
 
 def message_level(level:str):
-    if level == "Niveau 1":
+    if level == BEGINNER:
         return "Attention votre score est assez bas, vous tombez facilement dans le piège, nous vous conseillons fortement de vous inscrire pour améliorer votre niveau"
-    elif level == "Niveau 2":
+    elif level == AVERAGE:
         return "Votre score est moyen, nous vous conseillons de lire nos conseils et de vous inscrire pour améliorer votre niveau et ne plus tomber dans le piège des cybercriminels"
-    elif level == "Niveau 3":
+    elif level == ADVANCED:
         return "Vous avez un bon score, vous tombez rarement dans le piège, nous vous conseillons toutefois de vous inscrire ou de lire nos conseils pour rester sur vos gardes."
 
 def evaluate_level_test(score):
     assert 0<= score <= 10
     if score < 3:
-        return "Niveau 1"
+        return BEGINNER
     elif score < 6:
-        return "Niveau 2"
+        return AVERAGE
     else:
-        return "Niveau 3"
+        return ADVANCED
